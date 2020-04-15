11e72ce1ce7c1d3b498544fa310c3eac69c43026
이상훈 <dl57934@naver.com>
2020-04-16 05:08:32 +0900

    first made golang http Server

---
+++ back/index.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+type HttpHandler struct {
+}
+
+func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Hello World")
+	res.Write(data)
+}
+
+func main() {
+	handler := HttpHandler{}
+	http.ListenAndServe(":9000", handler)
+}
